9a7f9000e5181f3b13ed1880c571a0ebd1298b5a
Giuseppe Tortorelli <10582962@polimi.it>
2021-04-23 13:27:21 +0200

    Added main pages

--- pages/areas.vue
+++ pages/areas.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
+    <h1>Areas</h1>
+    <nuxt-link to="/areas"></nuxt-link>
     <a href="/company">Company</a>
     <a href="/contacts">Contacts</a>
-    <a href="/areas">Areas</a>
     <a href="/products">Products</a>
     <a href="/roles">Roles</a>
+    <a href="/">Home</a>
   </div>
 </template>
--- pages/company.vue
+++ pages/company.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
-    <h1>Home</h1>
-    <a href="/company">Company</a>
+    <h1>Company</h1>
+    <nuxt-link to="/company"></nuxt-link>
     <a href="/contacts">Contacts</a>
     <a href="/areas">Areas</a>
     <a href="/products">Products</a>
     <a href="/roles">Roles</a>
+    <a href="/">Home</a>
   </div>
 </template>
