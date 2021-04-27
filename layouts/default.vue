08df3f3b7622084c4b23d0cf55163e02f4f1d687
Giuseppe Tortorelli <10582962@polimi.it>
2021-04-27 20:58:05 +0200

    All areas page partially done

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,56 +1,58 @@
 <template>
   <div>
-    <!-- Add header -->
+    <!-- <the-header></the-header> -->
     <Nuxt />
+    <!-- <the-footer></the-footer> -->
   </div>
 </template>
 
+<script>
+import TheHeader from '~/components/TheHeader.vue'
+import TheFooter from '~/components/TheFooter.vue'
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+}
+</script>
+
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
+  font-family: 'Roboto', sans-serif;
   box-sizing: border-box;
 }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
+.container {
+  padding: 10px;
 }
 
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+section{
+  display: flex;
+  padding: 50px;
+  flex-direction: row;
 }
 
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+button {
+  text-align: center;
+  height: 50px;
+  padding-inline: 30px;
+  border-radius: 5px;
 }
 
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
+button.strong {
+  color: white;
+  border: none;
+  background-color: black;
 }
 
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
+button.light {
+  color: black;
+  border-color: black;
+  border-radius: 5px;
+  background-color: white;
 }
+
+
 </style>
+
--- pages/areas/index.vue
+++ pages/areas/index.vue
@@ -1,1 +1,75 @@
-<!--ALL AREAS -->
+<template>
+  <main class="container">
+    <section class="intro">
+      <div class="intro-text">
+        <h1>OUR AREAS</h1>
+        <p>Short overview of the 4 different areas</p>
+        <button class="strong">Go to all areas</button>
+      </div>
+      <div class="intro-image">
+        <img src="~/static/areas/mokup_areas-1.png">
+      </div>
+    </section>
+    <section class="accelerators">
+      <h3>Areas</h3>
+      <p>Introductory text of the 4 areas</p>
+      <div class="links">
+        <!-- TODO: generare dinamicamente -->
+        <button class="strong">Area1</button>
+        <button class="strong">Area2</button>
+        <button class="strong">Area3</button>
+        <button class="strong">Area4</button>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default{
+  async asyncData({ $axios }) {
+    const { data } = await $axios.get("${process.env.BASE_URL}/api/areas")
+    const areas = data
+    return {
+      areas,
+    }
+  },
+
+}
+</script>
+
+
+<style scoped>
+div.intro-text {
+  flex-grow: 2;
+  padding-top: 100px;
+}
+div.intro-image {
+  flex-grow: 3;
+  align-content: center;
+}
+
+div.intro-image img{
+  max-width: 1000px;
+  height: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto 
+}
+
+section.accelerators{
+  flex-direction: column;
+  text-align: center;
+  background: #f2f2f2;
+}
+
+.links {
+  display: flex;
+  width: 600px;
+  justify-content: space-between;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+}
+</style>
