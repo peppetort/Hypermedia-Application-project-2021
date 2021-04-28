b8a3ef4d7c8526315ab7fc33ebb3e41872ea6e86
simone cattaneo <simone6.cattaneo@mail.polimi.it>
2021-04-28 09:06:31 +0200

    api implementation

--- server/rest/api.js
+++ server/rest/api.js
@@ -10,21 +10,56 @@ async function init() {
   // Call the init function that returns the Database
   const db = await initializeDatabase();
   // Let's extract all the objects we need to perform queries inside the endpoints
-  // eslint-disable-next-line no-underscore-dangle
-  const { Person, Area, Product, Image, Assistance } = db._tables;
+  const { Person, Area, Products, Image, Assistance, Features } = db._tables;
+
+  //api to get all the products
+  app.get('api/products', async (req, res) => {
+    const products = await Product.findAll();
+    return res.json(products);    
+  });
+  //api to get all the people
+  app.get('api/people', async (req, res) => {
+    const people = await Person.findAll();
+    return res.json(people);
+  });
+  //api to get all the areas
+  app.get('api/areas', async (req, res) => {
+    const areas = await Area.findAll();
+    return res.json(areas);
+  });
   // API to get all products by area
+  //the parameter :area passed is the id of the area from which we want to retrieve all the products
   app.get('api/products/:area', async (req, res) => {
-    const { area } = req.params;
-    const products = await Product.findAll(/* TODO: query for all product of an area */);
+    const area  = req.params.area;
+    const products = await Product.findAll({
+      where: {area: area},
+    });
     return res.json(products);
   });
   // API to get all people by role
+  // the parameter :role passed is the identifier of the role consistent with the representation in the db
+  // => 0: area rep, 1: proj manager, 2: ref assistant
   app.get('api/:role', async (req, res) => {
-    const { role } = req.params;
-    const people = await Person.findAll(/* TODO: query for all people of a role */);
+    const role = req.params.role;
+    const people = await Person.findAll({
+      where: {role: role},
+    });
     return res.json(people);
   });
-  /* TODO: complete with necessary API */
+  // api to get all the roles of a person
+  app.get('api/roles/:person', async (req, res) => {
+    const person = req.params.person;
+    const area_resp = await Area.findOne({
+      where: {manager: person},
+    });
+    const proj_manager = await Product.findAll({
+      where: {project_manager: person},
+    })
+    const ref_ass = await Product.findAll(/*find all products for which perso is ref assistant*/);
+
+    return res.json({area_resp, proj_manager, ref_ass});
+  })
+  
 }
 
 init();
