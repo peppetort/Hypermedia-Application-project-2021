83786637f4c092309287e7e3699615e142a5f34c
Giuseppe Tortorelli <10582962@polimi.it>
2021-05-07 16:56:10 +0200

    All products done

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,1 +1,48 @@
-<!--ALL PRODUCTS -->
+<template>
+  <main class="container">
+    <section class="vertical light">
+      <h1>Our Products</h1>
+      <p>Descrizione</p>
+      <!-- TODO: insert image -->
+    </section>
+    <section class="vertical light">
+      <h2>Select your Area</h2>
+      <div class="links">
+        <NuxtLink
+          v-for="area in areas"
+          :key="area.id"
+          :to="`products/area/${area.id}`"
+          ><button class="strong">
+            {{ area.title }}
+          </button></NuxtLink
+        >
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      areas: ''
+    }
+  },
+  async mounted() {
+    const { data } = await this.$axios.get('/api/areas')
+    this.areas = data
+  }
+}
+</script>
+
+<style scoped>
+div.links {
+  display: flex;
+  width: 600px;
+  justify-content: space-between;
+  padding: 5px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+}
+</style>
