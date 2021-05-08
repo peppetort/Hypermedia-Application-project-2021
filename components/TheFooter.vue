85239d47eefcd2290ea515feb0070546b0653869
Giuseppe Tortorelli <10582962@polimi.it>
2021-05-08 13:32:32 +0200

    footer done

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,85 +1,119 @@
 <template>
   <div class="footer">
-    <div class="top_bar">
-      <h2 class="discover_more">Want to discover more?</h2>
-      <a class="discover_more_button"
-        ><button class="light">Discover more</button></a
+    <div class="content">
+      <h2>Want to discover more?</h2>
+      <NuxtLink to="/contacts"
+        ><button class="light">Contact Us</button></NuxtLink
       >
     </div>
-    <div class="bottom_bar">
-      <div class="copyright_section">
+    <div class="content">
+      <div class="right">
         <h3>NAME</h3>
-        <p style="font-size = 1em">Copyright 2021</p>
-        <a>Privacy - Terms</a>
+        <p>2010 - 2021</p>
+        <p>Privacy terms</p>
       </div>
-      <div class="bottom_links">
-        <ul class="link_section">
-          <li><h4 class="section_title">Company</h4></li>
-          <li class="menu_item"><a>Who we are</a></li>
-          <li class="menu_item"><a>Partners</a></li>
-          <li class="menu_item"><a>Our values</a></li>
-        </ul>
-        <ul class="link_section">
-          <li><h4 class="section_title">Contacts</h4></li>
-          <li class="menu_item"><a>Contact us</a></li>
-          <li class="menu_item"><a>Phone number</a></li>
-          <li class="menu_item"><a>Visit us</a></li>
-        </ul>
-        <ul class="link_section">
-          <li><h4 class="section_title">Roles</h4></li>
-          <li class="menu_item"><a>Responsibles</a></li>
-          <li class="menu_item"><a>Project managers</a></li>
-          <li class="menu_item"><a>References</a></li>
-        </ul>
-        <ul class="link_section">
-          <li><h4 class="section_title">Areas</h4></li>
-          <li class="menu_item"><a>Network</a></li>
-          <li class="menu_item"><a>Security</a></li>
-          <li class="menu_item"><a>IOT</a></li>
-        </ul>
+      <div class="left">
+        <div class="column">
+          <NuxtLink to="/areas" class="title">Areas</NuxtLink>
+          <NuxtLink
+            v-for="area in areas"
+            :key="area.id"
+            :to="`/areas/${area.id}`"
+            >{{ area.title }}</NuxtLink
+          >
+        </div>
+        <div class="column">
+          <NuxtLink to="/roles" class="title">Roles</NuxtLink>
+          <NuxtLink to="/roles/0">Area Responsible</NuxtLink>
+          <NuxtLink to="/roles/1">Project Manager</NuxtLink>
+          <NuxtLink to="/roles/2">Assistence Referent</NuxtLink>
+        </div>
+        <div class="column">
+          <NuxtLink to="/products" class="title">Products</NuxtLink>
+          <NuxtLink
+            v-for="area in areas"
+            :key="area.id"
+            :to="`/products//areas/${area.id}`"
+            >{{ area.title }}</NuxtLink
+          >
+        </div>
+        <div class="column">
+          <NuxtLink to="/contacts" class="title">Contacts</NuxtLink>
+          <NuxtLink to="/contacts/#form">Contact us</NuxtLink>
+          <NuxtLink to="/contacts/#number">Phone Number</NuxtLink>
+          <NuxtLink to="/contacts/#map">Visit us</NuxtLink>
+        </div>
+        <div class="column">
+          <NuxtLink to="/company" class="title">Company</NuxtLink>
+          <NuxtLink to="/compnayy/#">Who we are</NuxtLink>
+          <!-- TODO: completare il link -->
+          <NuxtLink to="/company/#">Partners</NuxtLink>
+          <!-- TODO: completare il link -->
+          <NuxtLink to="/company/#">Our values</NuxtLink>
+          <!-- TODO: completare il link -->
+        </div>
       </div>
     </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      areas: ''
+    }
+  },
+  async mounted() {
+    const { data } = await this.$axios.get('/api/areas')
+    this.areas = data
+  }
+}
+</script>
+
 <style scoped>
-ul.link_section {
-  display: inline-block;
-  padding-right: 2em;
-  padding-left: 2em;
-  list-style-type: none;
-}
-li.menu_item {
-  margin: 1.2em 0;
-}
-
-div.bottom_links {
-  float: right;
-  padding-right: 2em;
-}
-
-div.copyright_section {
-  display: inline-block;
-}
-
-h4.section_title {
-  margin-bottom: 1.2em;
-}
-
-h2.discover_more {
-  display: inline-block;
-}
-
-button.light {
-  float: right;
-}
-
-a.discover_more_button {
-  float: right;
-  vertical-align: middle;
-}
-
 div.footer {
-  margin-top: 5em;
+  padding: 10px;
+  background-color: #f2f2f2;
+  display: flex;
+  flex-direction: column;
+}
+
+div.content {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-inline: 100px;
+  padding-top: 100px;
+  padding-bottom: 50px;
+}
+
+div.content h2 {
+  font-weight: bold;
+}
+
+div.left {
+  min-width: 900px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+
+div.column {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: left;
+  height: 150px;
+}
+
+div.column p {
+  font-size: 16px;
+}
+
+div.column .title {
+  font-weight: bold;
+  margin-bottom: 10px;
 }
 </style>
